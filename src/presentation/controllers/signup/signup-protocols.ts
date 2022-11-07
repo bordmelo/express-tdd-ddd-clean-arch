3b2e8977a7c962447e12206320fbf030ead3d019
Lucas Melo <lucas.melo@oncar.com.br>
2022-11-07 14:15:43 -0300

    refactor: change models directory

--- src/presentation/controllers/signup/signup-protocols.ts
+++ src/presentation/controllers/signup/signup-protocols.ts
@@ -1,4 +1,4 @@
 export * from '../../protocols'
 export * from '../../protocols/email-validator'
 export * from '../../../domain/use-cases/add-account'
-export * from '../../../domain/use-cases/models/account'
+export * from '../../../domain/models/account'

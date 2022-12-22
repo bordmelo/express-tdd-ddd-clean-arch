7f8c98d4d4d1360c12bd667d4ab9c0405df972c0
Lucas Melo <lucas.melo@oncar.com.br>
2022-12-21 21:27:07 -0300

    feat: ensure requests parse body as json

---
+++ src/main/config/app.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+import setupMiddleware from './middleware'
+
+const app = express()
+setupMiddleware(app)
+
+export default app
--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,3 @@
-import express from 'express'
+import app from './config/app'
 
-const app = express()
 app.listen(3000, () => console.log('Server is running at localhost:3000'))

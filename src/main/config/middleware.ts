49b82e550f1ae2eff2f16be58ffafecf7d4f0418
Lucas Melo <lucas.melo@oncar.com.br>
2022-12-22 09:25:06 -0300

    refactor: move all export middleware to index

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,8 +1,8 @@
 import { Express } from 'express'
-import { bodyParser } from '../middleware/body-parser'
-import { cors } from '../middleware/cors'
+import { bodyParser, cors, contentType } from '../middleware'
 
 export default (app: Express): void => {
   app.use(bodyParser)
   app.use(cors)
+  app.use(contentType)
 }

658fd332000426ac7a02cb5c77ee5800d388abde
Lucas Melo <lucas.melo@oncar.com.br>
2022-12-22 10:19:50 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,3 +1,9 @@
-import app from './config/app'
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
 
-app.listen(3000, () => console.log('Server is running at localhost:3000'))
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`Server is running at localhost:${env.port}`))
+  })
+  .catch(console.error)
